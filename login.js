7bf1eb23c48998732b7c07d9cef6973126cf9a6f
Danny <parkzb3ast@kau.kr>
2022-11-27 11:14:08 +0900

    정보 입력

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
