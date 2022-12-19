1439753c17ce2b783bd479ee4b8f934ee33e72f7
eunju lee <lgleunju217@gmail.com>
2022-12-19 16:44:59 +0900

    Merge branch 'master' of https://github.com/Tesssssssssy/WEB-Doosan

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-import pandas as pd
+# import pandas as pd
 
 auth = Blueprint('auth', __name__)
 
